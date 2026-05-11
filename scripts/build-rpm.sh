e375aced68b616e0fe1d026001a28f524d167731
Paul Draper <paul@getredo.com>
2026-05-10 18:28:05 -0600

    Map dashes to tildes for RPM pre-release versions

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rpm_version="${VERSION//-/~}"
+
 cargo generate-rpm --target "$TARGET" --payload-compress zstd \
-  --output "dist/$BINARY-$VERSION-1.$RPM_ARCH.rpm"
+  -s "version=\"$rpm_version\"" \
+  --output "dist/$BINARY-$rpm_version-1.$RPM_ARCH.rpm"
